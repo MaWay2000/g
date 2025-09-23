84446f868cd3f50b4adfa452469d9bc48a363dac
MM <mantasmatuseviciuz@gmail.com>
2025-09-23 04:32:11 +0300

    aa

--- public/config.js
+++
@@ -1,9 +0,0 @@
-window.GAME_CONFIG = {
-  /**
-   * When hosting the client separately from the Socket.IO server, set this to the
-   * full URL of the backend, for example:
-   *   serverUrl: 'https://multiplayer-cubes.onrender.com'
-   * Leave as null to use the same origin as the current page.
-   */
-  serverUrl: null,
-};
