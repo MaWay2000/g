cee652f5d9532fb1d3631e569b0e5f44d7cee60e
MM <mantasmatuseviciuz@gmail.com>
2025-12-04 03:59:35 +0200

    Reduce star extent multiplier

--- scripts/scene.js
+++ scripts/scene.js
@@ -80,7 +80,7 @@ export const initScene = (
     showStars: settings?.showStars !== false,
   };
 
-  const STAR_EXTENT_MULTIPLIER = 100;
+  const STAR_EXTENT_MULTIPLIER = 10;
 
   const registeredStarFields = new Set();
   const registerStarField = (starField) => {
