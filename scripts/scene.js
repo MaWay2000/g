f0c19a35ffcbe55f18ff5d408463b84d734baac0
MM <mantasmatuseviciuz@gmail.com>
2026-01-24 05:39:06 +0200

    Revert "Reduce outside terrain z-fighting"

--- scripts/scene.js
+++ scripts/scene.js
@@ -3928,9 +3928,6 @@ export const initScene = (
           map: texture ?? null,
           transparent: false,
           opacity: terrainStyle.opacity,
-          polygonOffset: true,
-          polygonOffsetFactor: -1,
-          polygonOffsetUnits: -1,
         });
         terrainMaterials.set(materialKey, material);
         return material;
