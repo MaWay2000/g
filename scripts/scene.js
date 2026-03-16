ae6aaadbfdd4dcebf3b699794eabec3ebea5f5d5
MaWay2000 <mantasmatuseviciuz@gmail.com>
2026-03-16 03:25:40 +0200

    Revert "Add engineering bay roof collision"
    This reverts commit fa6b9382efeda2c7f6336423fdd7d8576a56eae6.

--- scripts/scene.js
+++ scripts/scene.js
@@ -9817,7 +9817,6 @@ export const initScene = (
     });
     const consoleGroups = [leftConsole.group, rightConsole.group];
     const environmentColliderDescriptors = [
-      { object: ceiling, ceilingOnly: true },
       { object: commandTableBase },
       { object: commandTableTop },
       { object: commandTableInset },
